7b96e4f85d2101fa5d047c2621075dcb4123edab
Juya <96727107+jUyA85Sg@users.noreply.github.com>
2022-01-05 10:07:45 +0100

    Mensajes más o menos

--- Proyecto final - app mensajería/js/main.js
+++ Proyecto final - app mensajería/js/main.js
@@ -1,19 +1,43 @@
 function nuevoCanal() {
-    var newName = document.getElementById("nuevo-canal-barra").value;
-    var listNode = document.getElementById("listacanales");
+    var nuevoCanal = document.getElementById("nuevo-canal-barra").value;
+    var txtNode = document.createTextNode(nuevoCanal);
+    var listNode = document.getElementById("lista-canales");
     var liNode = document.createElement("li");
     var aNode = document.createElement("a");
-    var txtNode = document.createTextNode(newName);
     var attr = document.createAttribute("href");
 
     attr.value = "#"; 
 
     aNode.setAttributeNode(attr);
 
-    aNode.appendChild(txtNode);
-    liNode.appendChild(aNode);
     listNode.appendChild(liNode);
+    liNode.appendChild(aNode);
+    aNode.appendChild(txtNode);
 
     document.getElementById("nuevo-canal-barra").value = "";
+
+}
+
+
+function nuevoMensaje() {
+    var nuevoMensaje = document.getElementById("nuevo-mensaje-barra").value;
+    var nombreUser = document.getElementById("nombre-user-barra").value;
+    var msjNode = document.createTextNode(nuevoMensaje);
+    var userNode = document.createTextNode(nombreUser);
+    var listNode = document.getElementById("zona-chat");
+    var liNode = document.createElement("div");
+    var pNode = document.createElement("p");
+    var uNode = document.createElement("strong");
+    var qNode = document.createElement("q");
+    
+
+    listNode.appendChild(liNode, pNode);
+    liNode.appendChild(qNode);
+    pNode.appendChild(uNode);
+    qNode.appendChild(msjNode);
+    uNode.appendChild(userNode); /* NADA, ESTO NO SÉ HACERLO, o consigo el mensaje o el usuario... y no sé cómo hacer para que tengan el formato que yo quiero. Ah, y le tengo que meter la clase "caja-mensajes"*/
+
+    document.getElementById("nuevo-mensaje-barra").value = "";
+    document.getElementById("nombre-user-barra").value = "";
 
 }
