82d89a7a5eba092806a2598effc9b5adfb52c407
Juya <96727107+jUyA85Sg@users.noreply.github.com>
2022-01-05 12:36:04 +0100

    Trabajando user y msjs

--- Proyecto final - app mensajería/js/main.js
+++ Proyecto final - app mensajería/js/main.js
@@ -1,43 +1,53 @@
 function nuevoCanal() {
-    var nuevoCanal = document.getElementById("nuevo-canal-barra").value;
-    var txtNode = document.createTextNode(nuevoCanal);
-    var listNode = document.getElementById("lista-canales");
-    var liNode = document.createElement("li");
-    var aNode = document.createElement("a");
+    var canalLoc = document.getElementById("nuevo-canal-barra").value;
+    var canalNuevo = document.createTextNode(canalLoc);
+    var listaLoc = document.getElementById("lista-canales");
+    var listaNuevo = document.createElement("li");
+    var aNuevo = document.createElement("a");
     var attr = document.createAttribute("href");
 
     attr.value = "#"; 
 
-    aNode.setAttributeNode(attr);
+    aNuevo.setAttributeNode(attr);
 
-    listNode.appendChild(liNode);
-    liNode.appendChild(aNode);
-    aNode.appendChild(txtNode);
+    listaLoc.appendChild(listaNuevo);
+    listaNuevo.appendChild(aNuevo);
+    aNuevo.appendChild(canalNuevo);
 
     document.getElementById("nuevo-canal-barra").value = "";
 
 }
 
-
-function nuevoMensaje() {
-    var nuevoMensaje = document.getElementById("nuevo-mensaje-barra").value;
-    var nombreUser = document.getElementById("nombre-user-barra").value;
-    var msjNode = document.createTextNode(nuevoMensaje);
-    var userNode = document.createTextNode(nombreUser);
-    var listNode = document.getElementById("zona-chat");
-    var liNode = document.createElement("div");
-    var pNode = document.createElement("p");
-    var uNode = document.createElement("strong");
-    var qNode = document.createElement("q");
+function nombreUser() {
+    var userLoc = document.getElementById("nombre-user-barra").value;
+    var userNuevo = document.createTextNode(userLoc);
+    var listaLoc = document.getElementById("zona-user"); /* no existe aún zona-user (de hecho, no "getElement": esto no será una list, tiene que sustituir (aunque lo tengo que crear primero) y entonces lo de abajo creo que tampoco me hace falta si encuentro el "sustituir"*/
+    var listaNuevo = document.createElement("div");
+    var pNuevo = document.createElement("p");
+    var uNuevo = document.createElement("strong");
     
+/* Revisar qué me vale de aquí; y del document, a ver cómo llamo a la "barra" -imagino que igual- */
+    listaLoc.appendChild(listaNuevo, pNuevo);
+    listaNuevo.appendChild(qNode);
+    pNuevo.appendChild(uNuevo);
+    uNuevo.appendChild(userNuevo);
 
-    listNode.appendChild(liNode, pNode);
-    liNode.appendChild(qNode);
-    pNode.appendChild(uNode);
-    qNode.appendChild(msjNode);
-    uNode.appendChild(userNode); /* NADA, ESTO NO SÉ HACERLO, o consigo el mensaje o el usuario... y no sé cómo hacer para que tengan el formato que yo quiero. Ah, y le tengo que meter la clase "caja-mensajes"*/
-
-    document.getElementById("nuevo-mensaje-barra").value = "";
     document.getElementById("nombre-user-barra").value = "";
 
+}
+
+function nuevoMensaje() {
+    var mensajeLoc = document.getElementById("nuevo-mensaje-barra").value;
+    var mensajeNuevo = document.createTextNode(mensajeLoc);
+    var listaLoc = document.getElementById("zona-chat");
+    var listaNuevo = document.createElement("div");
+    var pNuevo = document.createElement("p");
+    var qNuevo = document.createElement("q");
+
+    listaLoc.appendChild(listaNuevo);
+    listaNuevo.appendChild(pNuevo);
+    pNuevo.appendChild(qNuevo);
+    qNuevo.appendChild(mensajeNuevo);
+
+    document.getElementById("nuevo-mensaje-barra").value = "";
 }
